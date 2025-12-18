4978f3c23f886837557b55733941b75b8393dd43
Nic Laflamme <nic@laflamme.io>
2025-12-18 03:22:57 -0700

    feat: add confirmation prompt to dotenv command

--- src/commands/print_dot_env.rs
+++ src/commands/print_dot_env.rs
@@ -1,9 +1,18 @@
 use crate::db::DbManager;
-use crate::shared::{get_effective_env, get_env_slug, get_project_from_current_dir};
+use crate::shared::{
+    ask_for_confirmation, get_effective_env, get_env_slug, get_project_from_current_dir,
+};
 
 pub fn print_dot_env(db_manager: &DbManager, env: Option<String>) {
     if let Some(project) = get_project_from_current_dir(db_manager) {
         let env_slug = get_env_slug(db_manager, &env, &project.id);
+
+        if !ask_for_confirmation(&format!(
+            "You are about to print secrets for environment '{}'. Are you sure?",
+            env_slug
+        )) {
+            return;
+        }
 
         match get_effective_env(db_manager, &project, &env_slug) {
             Ok(env_vars) => {
